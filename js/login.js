5d8699c5374e582a8fabff9d266b5ed97b58b602
Daniel Vitorio <dvitorio2@gmail.com>
2022-02-19 00:06:55 -0300

    Primeiro commit: construção da primeira página e tela de login

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function logar(){
+    let abrirLogin = document.getElementById("areaLogin");
+    abrirLogin.style.visibility = 'visible';
+}
+
+function deslogar(){
+    let abrirLogin = document.getElementById("areaLogin");
+    abrirLogin.style.visibility = 'hidden';
+}
