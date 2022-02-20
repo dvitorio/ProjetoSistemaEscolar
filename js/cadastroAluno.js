19e70f27938e8f1e26121fcfb7fc71954f34daa7
Daniel Vitorio <dvitorio2@gmail.com>
2022-02-20 13:37:31 -0300

    Início da construção do Portal do Aluno

--- js/cadastroAluno.js
+++ js/cadastroAluno.js
@@ -1,3 +1,13 @@
 function paginaInicial(){
     window.open("index.html");
 }
+
+function abrirPortalAluno(){
+    let abrir = document.querySelector("#portalAluno");
+    abrir.style.visibility = 'visible';
+}
+
+function fecharPortalAluno(){
+    let fechar = document.querySelector("#portalAluno");
+    fechar.style.visibility = 'hidden';
+}
