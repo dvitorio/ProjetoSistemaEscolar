b4c666d0ea6aeae50f1e6742a84f7762caf22351
Daniel Vitorio <dvitorio2@gmail.com>
2022-03-03 22:04:59 -0300

    Commit inicial

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ function logar(){
     const usuario = document.getElementById("usuario").value;
     const senha = document.getElementById("senha").value;
         if((usuario == "") && (senha == "")){
-            window.open("cadastroAluno.html");
+            window.open("portais.html");
             window.close("login.html");
             window.close("index.html");
         }
